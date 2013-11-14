a7ee5f320dedaed2c95826e1330f774e7fa3f2bb
khaled777b <beddakhe khaled>
2013-11-14 20:07:14 +0100

    base rails app added status

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dzlinks::Application.config.secret_key_base = '1e69bc37ea61825402a9ddf059cef2d6c8f126ec3c9a66fcb3fc39ac672257dc5971f665f90e60f6b9161f64c887d5dcfbfa41955d43ca2b977f6835f38de744'
